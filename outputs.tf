472f7124991e80d42849e9c451a9a95cd45b15ba
Jedrzej <jedrzej@passion.io>
2020-10-15 10:18:19 +0200

    fix

--- outputs.tf
+++ outputs.tf
@@ -12,7 +12,7 @@ output "alert_arn" {
   value       = aws_cloudformation_stack.sns-topic-alert.outputs["ARN"]
   description = "Email alert SNS topic ARN"
 }
-output "alert_arn" {
+output "alert_topic" {
   value       = aws_cloudformation_stack.sns-topic-alert.outputs["TopicName"]
   description = "Email alert SNS topic name"
 }
